b1c3a5786f33a257d45a7e0e38ce11285712dedd
Yousuf <mmohdyusuf888@gmail.com>
2021-01-07 11:02:15 +0530

    second python program

---
+++ django_project.py
@@ -0,0 +1,37 @@
+# Create the project directory
+mkdir tutorial
+cd tutorial
+
+# Create a virtual environment to isolate our package dependencies locally
+python3 -m venv env
+source env/bin/activate  # On Windows use `env\Scripts\activate`
+
+# Install Django and Django REST framework into the virtual environment
+pip install django
+pip install djangorestframework
+
+# Set up a new project with a single application
+django-admin startproject tutorial .  # Note the trailing '.' character
+cd tutorial
+django-admin startapp quickstart
+cd ..
+The project layout should look like:
+
+$ pwd
+<some path>/tutorial
+$ find .
+.
+./manage.py
+./tutorial
+./tutorial/__init__.py
+./tutorial/quickstart
+./tutorial/quickstart/__init__.py
+./tutorial/quickstart/admin.py
+./tutorial/quickstart/apps.py
+./tutorial/quickstart/migrations
+./tutorial/quickstart/migrations/__init__.py
+./tutorial/quickstart/models.py
+./tutorial/quickstart/tests.py
+./tutorial/quickstart/views.py
+./tutorial/settings.py
+./tutorial/url
